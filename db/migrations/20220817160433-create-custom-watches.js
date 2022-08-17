4300494702797e81be43dcc64dc7f4e3b1a74832
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-17 19:13:13 +0300

    Create model CustomWatch

---
+++ db/migrations/20220817160433-create-custom-watches.js
@@ -0,0 +1,44 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const attributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      watchName: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      picture: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    };
+
+    await queryInterface.createTable("CustomWatch", attributes);
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable("CustomWatch");
+  },
+};
