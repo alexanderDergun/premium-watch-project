beb3fc7f7de62b85cea2313452bf3195f31b7886
Oskar <oskar.stud@gmail.com>
2022-08-17 18:12:25 +0300

    rout for test navbar

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('@babel/register');
 const app = require('express')();
 const Config = require('./config/config');
+const Layout = require('./views/Layout');
+const Header = require('./views/Header.jsx');
 // Импортируем роуты
 
 Config(app);
 const PORT = process.env.PORT ?? 3000;
 
 // Подключаем роуты
+app.get('/', (req,res) => {
+    res.renderComponent(Header, {})
+})
 
 app.listen(PORT, () => console.log('Секрет успеха — сделать первый шаг'));
