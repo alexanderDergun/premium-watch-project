c39fc03b0d4d19f422e477f1767e28d61d88d6cf
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-18 15:20:18 +0300

    Make LoginRout

--- routes/logaRegaRouters/login.rout.js
+++ routes/logaRegaRouters/login.rout.js
@@ -1,8 +1,24 @@
-const LoginRout = require('express').Router();
-const Login = require('../../views/LogReg/Login');
+const LoginRout = require("express").Router();
+const Login = require("../../views/LogReg/Login");
+const bcrypt = require('bcrypt')
+const { User } = require("../../db/models");
 
-LoginRout.get('/', (req, res) => {
+LoginRout.get("/", (req, res) => {
   res.renderComponent(Login, {});
+});
+
+LoginRout.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: { email },
+  });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    req.session.user = user;
+    res.redirect('/')
+  } else {
+    res.json({login: false})
+  }
 });
 
 module.exports = LoginRout;
