1d11ab650cc3ef662db88adaaf5ce61d2a6854f5
Oskar <oskar.stud@gmail.com>
2022-08-18 09:27:59 +0300

    Add card to main page

--- views/Card.jsx
+++ views/Card.jsx
@@ -3,8 +3,11 @@ const React = require('react');
 module.exports = function Card() {
     return (
         <div className="cardCointainer">
+            <div className='cardContainerImg'>
+                <img className="cardImg" src='/img/img1.png'></img>
+            </div>
             <span className="cardCointeainerHeader">Zoro Mindsweep G</span>
-            <span></span>
+            <span className="cardPrice">$169</span>
             <button>В корзину</button>
         </div>
     )
