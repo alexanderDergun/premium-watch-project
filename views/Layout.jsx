d1b429d281a71c61ea1b00edd2d8e30c5e715d1b
Alexander <raphop27@gmail.com>
2022-08-18 13:05:32 +0300

    create client auth scripts

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -10,12 +10,16 @@ module.exports = function Layout({ children, title, user }) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin="anonymous" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
         <title>{title}</title>
-        <link href="/css/styles.css" rel="stylesheet"></link>
+        <link href="/css/styles.css" rel="stylesheet" />
+        <script defer src="/js/client.js" />
+        <script defer src="/js/clientLogReg/client.reg.js" />
+        <script defer src="/js/clientLogReg/client.login.js" />
+
       </head>
       <body>
         <header>
           <nav className="navbar  navbar-light bg-light padding20px" id="navHeader">
-            <a className="nav-link" id="loginLink" href="#">{user}</a>
+            <a className="nav-link" id="loginLink" href="/">{user}</a>
             <a className="navbar-brand" href="/">Fixed top</a>
             <a className="nav-link" id="loginLink" href="/login">Войти</a>
             <a className="nav-link" id="regLink" href="/rega">Зарегестрироваться</a>
