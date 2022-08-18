a232250aec683d357dd754e8d9d6c66f56c9e2e0
Oskar <oskar.stud@gmail.com>
2022-08-18 12:42:35 +0300

    Style for MainCard

--- views/MainCard.jsx
+++ views/MainCard.jsx
@@ -2,8 +2,8 @@ const React = require('react');
 
 module.exports = function MainCard() {
     return (
-            <div class="sketchfab-embed-wrapper">
-                <iframe title="Apple Watch Series 7 - Free Watch-Face SDC™️" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="500" height="700" src="https://sketchfab.com/models/cf15ecbf159f42a28df5e56bdfa666bb/embed"> </iframe>
+            <div className="sketchfab-embed-wrapper">
+                <iframe title="Apple Watch Series 7 - Free Watch-Face SDC™️" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="300" height="550" src="https://sketchfab.com/models/cf15ecbf159f42a28df5e56bdfa666bb/embed"> </iframe>
                 
                     {/* <a href="https://sketchfab.com/3d-models/apple-watch-series-7-free-watch-face-sdctm-cf15ecbf159f42a28df5e56bdfa666bb?utm_medium=embed&utm_campaign=share-popup&utm_content=cf15ecbf159f42a28df5e56bdfa666bb" target="_blank"> Apple Watch Series 7 - Free Watch-Face SDC™️ </a> by <a href="https://sketchfab.com/3Duae?utm_medium=embed&utm_campaign=share-popup&utm_content=cf15ecbf159f42a28df5e56bdfa666bb" target="_blank"> SDC PERFORMANCE™️ </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=cf15ecbf159f42a28df5e56bdfa666bb" target="_blank">Sketchfab */}
                     {/* </a> */}
