2d0281ac9ec997d39bf0daaa456732b7105ce6c2
Oskar <oskar.stud@gmail.com>
2022-08-18 17:27:05 +0300

    add bcrypt for password

--- routes/logaRegaRouters/rega.rout.js
+++ routes/logaRegaRouters/rega.rout.js
@@ -1,23 +1,24 @@
 const RegaRout = require('express').Router();
 const Rega = require('../../views/LogReg/Rega');
 const { User } = require('../../db/models')
+const bcrypt = require('bcrypt');
 
 RegaRout.get('/', (req, res) => {
   res.renderComponent(Rega, {});
 });
 
 RegaRout.post('/', async (req, res) => {
-  console.log(req.body)
   
   try {
     const user = await User.create({
       username: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
-      password: req.body.password,
+      password: await bcrypt.hash(req.body.password, 10),
       isAdmin: false,
     })
 
+    req.session.user = user;
     res.redirect('/');
   } catch (error) {
     res.json({error: error.message, data: null});
