792e7dd15a1bf1a051b780ba3d259517506ede50
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-18 15:21:08 +0300

    Make client.login.js

---
+++ public/js/clientLogReg/client.login.js
@@ -0,0 +1,25 @@
+const { application } = require("express");
+
+const form = document.querySelector('#logForm');
+const btnLogin = document.querySelector('#btnLogin')
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const {email, password, action, method} = event.target
+    
+    const response = await fetch(action, {
+        method,
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify ({
+            email: email.value,
+            password: password.value
+        })
+    })
+
+    const data = await response.json();
+    if(!data.login) {
+        btnLogin.insertAdjacentHTML('beforebegin', '<div>Логин или пароль не верные</div>');
+    }
+
+})
