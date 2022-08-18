5f23c5ea287666e4ca913831eaa2ceb9ef8d6a9b
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-18 16:59:53 +0300

    Fix package.json

--- middleware/userCheck.js
+++ middleware/userCheck.js
@@ -1,7 +1,9 @@
 module.exports = function sessionCheck(req, res, next) {
+  // console.log(req.session.user);
   if (req.session.user) {
     res.locals.user = req.session.user;
   }
+
 
   next();
 };
