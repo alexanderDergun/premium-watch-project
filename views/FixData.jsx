9fb7d84d27b4f8785b74552f7ce6069e9322d9ae
Oskar <oskar.stud@gmail.com>
2022-08-19 15:43:19 +0300

    add style form to lk/editor

--- views/FixData.jsx
+++ views/FixData.jsx
@@ -1,29 +1,29 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function FixData ({user}) {
+module.exports = function FixData({ user }) {
     return (
         <Layout user={user}>
-        <form id='fixData' action="/lk/editor" name='fixData' method='post'>
-            
-            Изменить имя:<input name="name" type="text" value={user.username} required/>
-            <br />
-            Изменить Email:<input name="email" type="email" value={user.email} required/>
-            <br />
-            Изменить телефон:<input name="phone" type="tel" value={user.phone} required/>
-            <br />
-            Изменить пароль:<input name="password" type="password"/>
-            <br />
-            Повторите пароль:<input name="repeatPassword" type="password" />
+            <form id='fixData' action="/lk/editor" name='fixData' method='post'>
 
-            <br />
-            <br />
-            
-            <button type="submit">
-                Изменить
-            </button>
+                Изменить имя:<input name="name" type="text" value={user.username} required />
+                <br />
+                Изменить Email:<input name="email" type="email" value={user.email} required />
+                <br />
+                Изменить телефон:<input name="phone" type="tel" value={user.phone} required />
+                <br />
+                Изменить пароль:<input name="password" type="password" />
+                <br />
+                Повторите пароль:<input name="repeatPassword" type="password" />
 
-        </form>
+                <br />
+                <br />
+
+                <button type="submit" className="btn btn-success">
+                    Изменить
+                </button>
+
+            </form>
         </Layout>
     )
 }
