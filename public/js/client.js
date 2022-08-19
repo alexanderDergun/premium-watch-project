422b527e10c5680c18bde6c803d370c60f8119f8
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-19 16:23:01 +0300

    Fix client files

--- public/js/client.js
+++ public/js/client.js
@@ -1,57 +1,33 @@
-
-document.querySelector('.catalogList').addEventListener('click', async (event) => {
-
-    event.preventDefault();
-    const id = Number(event.target.dataset.id);
-    const response = await fetch(`/${id}/modal`);
-    const html = await response.text();
-    console.log(html);
-    if (html.data === null) {
-
-    } else {
-        document.body.insertAdjacentHTML('beforeend', html)
-
-    }
-
-    document.querySelector('[data-bs-dismiss="modal"]').addEventListener('click', (modalEvent) => {
-        modalEvent.target.closest('.modal').remove();
-    });
-
-
-// const lkInfo = document.querySelector('#lk');
 const fixData = document.querySelector('#fixData');
 
+if (fixData) {
+    fixData.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const { action, name, email, phone, password, repeatPassword } = event.target
 
 
-fixData.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const {action, name, email, phone, password, repeatPassword} = event.target
-
-
-    if(password.value && password.value === repeatPassword.value) {
-        const response = await fetch(action, {
-            method: "PUT",
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify ({
-                name: name.value,
-                email: email.value,
-                phone: phone.value,
-                password: password.value,
+        if (password.value && password.value === repeatPassword.value) {
+            const response = await fetch(action, {
+                method: "PUT",
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({
+                    name: name.value,
+                    email: email.value,
+                    phone: phone.value,
+                    password: password.value,
+                })
             })
-        }) 
-    } else {
-        const response = await fetch(action, {
-            method: "PUT",
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify ({
-                name: name.value,
-                email: email.value,
-                phone: phone.value,
+        } else {
+            const response = await fetch(action, {
+                method: "PUT",
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({
+                    name: name.value,
+                    email: email.value,
+                    phone: phone.value,
+                })
             })
-        }) 
-    }
-
-    
-
-})
+        }
+    })
+}
---
+++ public/js/modalClient.js
@@ -0,0 +1,26 @@
+document.querySelector('.catalogList').addEventListener('click', async (event) => {
+    event.preventDefault();
+    const id = Number(event.target.dataset.id);
+    const responseModal = await fetch(`/${id}/modal`);
+    const html = await responseModal.text();
+    // console.log(html);
+    if (html.data === null) {
+
+    } else {
+        document.body.insertAdjacentHTML('beforeend', html)
+        const deleteButton = document.querySelector(".deleteButton");
+
+            deleteButton.addEventListener("click", async (event) => {
+            // event.preventDefault();
+
+            const response = await fetch(`/${id}/modal`, {
+                method: "delete",
+            });
+            });
+    }
+
+    document.querySelector('[data-bs-dismiss="modal"]').addEventListener('click', (modalEvent) => {
+        modalEvent.target.closest('.modal').remove();
+    });
+});
+
