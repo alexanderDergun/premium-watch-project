56f2ced41358d3f61f75ec8eed0cd9a0083eafc8
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-19 12:32:23 +0300

    Other changes

--- public/js/client.js
+++ public/js/client.js
@@ -1,36 +1,31 @@
 // const lkInfo = document.querySelector('#lk');
-const fixData = document.querySelector('#fixData');
+const fixData = document.querySelector("#fixData");
 
+fixData.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
+  const { action, name, email, phone, password, repeatPassword } = event.target;
 
-fixData.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const {action, name, email, phone, password, repeatPassword} = event.target
-
-
-    if(password.value && password.value === repeatPassword.value) {
-        const response = await fetch(action, {
-            method: "PUT",
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify ({
-                name: name.value,
-                email: email.value,
-                phone: phone.value,
-                password: password.value,
-            })
-        }) 
-    } else {
-        const response = await fetch(action, {
-            method: "PUT",
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify ({
-                name: name.value,
-                email: email.value,
-                phone: phone.value,
-            })
-        }) 
-    }
-
-    
-})
+  if (password.value && password.value === repeatPassword.value) {
+    const response = await fetch(action, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        password: password.value,
+      }),
+    });
+  } else {
+    const response = await fetch(action, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+      }),
+    });
+  }
+});
