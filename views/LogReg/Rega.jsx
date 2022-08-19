7ce43f1f8bcc48878e8b5541cbea74e69c3b1964
Oskar <oskar.stud@gmail.com>
2022-08-19 10:50:11 +0300

    styles and required for Rega

--- views/LogReg/Rega.jsx
+++ views/LogReg/Rega.jsx
@@ -4,30 +4,32 @@ const Layout = require('../Layout');
 module.exports = function Rega() {
   return (
     <Layout>
+      <div className='regFormContainer'>
       <form id="regForm" action="/rega" method="POST">
         <label>
           {' '}
           Имя:
-          <input name="name" type="text" />
+          <input required name="name" type="text" />
         </label>
         <label>
           Email
-          <input name="email" type="email" />
+          <input required name="email" type="email" />
         </label>
         <label>
           Телефон:
-          <input name="phone" type="tel" />
+          <input required name="phone" type="tel" />
         </label>
         <label>
           Пароль:
-          <input name="password" type="password" />
+          <input required name="password" type="password" />
         </label>
         <label>
           Повторите пароль
-          <input name="reppassword" type="password" />
+          <input required name="reppassword" type="password" />
         </label>
         <button type="submit">Зарегистрироваться</button>
       </form>
+      </div>
     </Layout>
   );
 };
