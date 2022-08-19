5b69e5018d30a9659a24c05c35528d6feebe8b9b
Oskar <oskar.stud@gmail.com>
2022-08-19 14:39:48 +0300

    add hover to Card

--- views/Card.jsx
+++ views/Card.jsx
@@ -2,12 +2,12 @@ const React = require('react');
 
 module.exports = function Card({ card }) {
     return (
-        <div className="cardCointainer" data-id={card.id}>
+        <div rel="grow" className="cardCointainer grow" data-id={card.id}>
             {/* <div className='cardContainerImg' data-id={card.id}>
                 <img className="cardImg" src={card.picture} data-id={card.id}></img>
             </div> */}
             <img className="cardImg" src={card.picture} data-id={card.id}></img>
-            <h5><span className="cardCointeainerHeader" data-id={card.id}> {card.watchName}</span></h5> 
+            <h5><span className="cardCointeainerHeader" data-id={card.id}> {card.watchName}</span></h5>
             <h6><span className="cardPrice" data-id={card.id}> {card.price} </span></h6>
             <button id="cardBtn">В корзину</button>
         </div>
